3287294917a42d62b90e511afab35b4e5e619903
kalvner <bkalvner@gmail.com>
2020-12-03 13:29:24 -0400

    Link test

---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function Sobre() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/">
+        <a>Voltar para a HOme</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Sobre
