1b64dfd69b501ae53ed19972ba7a454569b14179
kalvner <bkalvner@gmail.com>
2020-12-03 13:32:02 -0400

    Link test 02

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
 function Sobre() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/">
-        <a>Voltar para a HOme</a>
+        <a>Voltar para a Home</a>
       </Link>
     </div>
   )
